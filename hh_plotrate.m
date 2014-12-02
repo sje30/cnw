c85e7844e3be980f27bccdbc8d3657e717f2e74f
Stephen J. Eglen <sje30@cam.ac.uk>
2014-12-02 09:49:58 +0000

    *** empty log message ***

---
+++ hh_plotrate.m
@@ -0,0 +1,13 @@
+% Example to fill in curve.
+I = zeros(4,1);
+R = zeros(4,1);
+
+I(1) = 0;   R(1) = 0;
+I(2) = 100; R(2) = 10;
+I(3) = 200; R(3) = 20;
+I(4) = 300; R(4) = 35;
+
+subplot(1,1,1);
+plot(I, R); 
+xlabel('input current (...)')
+ylabel('firing rate (Hz)')
