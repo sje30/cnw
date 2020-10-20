bd2140ba3d16d29754f8084c99d2364c8c38c059
Stephen J. Eglen <sje30@cam.ac.uk>
2020-10-20 22:21:52 +0100

    Convert to function and fix plotting of stimulus

--- izh.jl
+++ izh.jl
@@ -4,32 +4,35 @@
 
 using PyPlot
 
-#a=0.02; b=0.2;  c=-65;  d=6;
-V=-70;  u=b*V;
-VV=[];  uu=[];
-tau = 0.25; tspan = 0:tau:100;
-T1=tspan[end]/10;
-for t=tspan
-    if (t>T1)
-        I=14;
-    else
-        I=0;
+function izh(;a=0.02,b=0.2, c=-65.0, d=6.0)
+    #a=0.02; b=0.2;  c=-65;  d=6;
+    V=-70;  u=b*V;
+    VV=[];  uu=[];
+    tau = 0.25; tspan = 0:tau:100;
+    T1=tspan[end]/10;
+    for t=tspan
+        if (t>T1)
+            I=14;
+        else
+            I=0;
+        end;
+        V = V + tau*(0.04*V^2+5*V+140-u+I);
+        u = u + tau*a*(b*V-u);
+        if V > 30
+            push!(VV,30);
+            V = c;
+            u = u + d;
+        else
+            push!(VV,V);
+        end;
+        push!(uu,u);
     end;
-    V = V + tau*(0.04*V^2+5*V+140-u+I);
-    u = u + tau*a*(b*V-u);
-    if V > 30
-        push!(VV,30);
-        V = c;
-        u = u + d;
-    else
-        push!(VV,V);
-    end;
-    push!(uu,u);
-end;
-fig, axes = subplots(1,1,figsize=(5,3))
-plot(tspan,VV);
-plot([0,T1,T1,tspan[end]],-90+[0,0,10,10])
-title("(A) tonic spiking")
-xlim([0,tspan[end]])
-ylim([-90,30])
+    fig, axes = subplots(1,1,figsize=(5,3))
+    plot(tspan,VV);
+    plot([0,T1,T1,tspan[end]],-90.0 .+ [0,0,10,10])
+    title("(A) tonic spiking")
+    xlim([0,tspan[end]])
+    ylim([-90,30])
+end
+
 ##savefig("izhplot")
