e61919cd98e0908aa06b4b15217e428307050806
Stephen J. Eglen <sje30@cam.ac.uk>
2020-10-20 23:14:53 +0100

    Differential equation solver has changed...
    https://diffeq.sciml.ai/stable/tutorials/ode_example/ provides new examples.

--- myhh.jl
+++ myhh.jl
@@ -1,29 +1,31 @@
 using OrdinaryDiffEq
-
+using PyPlot
 include("hhode.jl")
           
 u0 = [-65.0;0.0529;0.3177;0.5961];
 tspan = (0.0,500.0)
-prob = ODEProblem(hhode,u0,tspan);
-sol = solve(prob,Vern7())
+p = [10]                       # input current
+prob = ODEProblem(hhode!,u0,tspan,p);
 
-using PyPlot
+sol = solve(prob,Vern7());
+
+
 fig, axes = subplots(2,2,figsize=(10,10))
 ax=axes[1,1]
-ax[:plot](sol.t,sol[1,:])
-ax[:set_xlabel]("t(ms)")
-ax[:set_ylabel]("V(mv)")
+ax.plot(sol.t,sol[1,:])
+ax.set_xlabel("t(ms)")
+ax.set_ylabel("V(mv)")
 
 ax=axes[1,2]
-ax[:plot](sol.t,sol[2,:])
-ax[:set_title]("m")
+ax.plot(sol.t,sol[2,:])
+ax.set_title("m")
 
 ax=axes[2,1]
-ax[:plot](sol.t,sol[3,:])
-ax[:set_title]("n")
+ax.plot(sol.t,sol[3,:])
+ax.set_title("n")
 
 ax=axes[2,2]
-ax[:plot](sol.t,sol[4,:])
-ax[:set_title]("h")
-fig[:subplots_adjust](hspace=0.35);
+ax.plot(sol.t,sol[4,:])
+ax.set_title("h")
+fig.subplots_adjust(hspace=0.35);
 show()
