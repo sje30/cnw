addc4da4507fdcab5af63220dba7ff4a97c1f02b
Stephen J. Eglen <sje30@cam.ac.uk>
2020-10-26 11:28:04 +0000

    make suitable for Plots.jl

--- euler1.jl
+++ euler1.jl
@@ -1,31 +1,41 @@
 # Simple example of Euler integration
 #h = 0.01;                               # time-step
-tmax = 5;                               # max t value
-init = 1;                               # initial condition
 
-t = 0:h:tmax;                           # vector of time values
-nsteps = length(t);                     # how many steps
-x = zeros(nsteps,1);                    # where we will store
-                                        # results
-x[1] = init;
+using Plots
 
-
-# Start the integration
-for i=1:(nsteps-1)
-    f = t[i] - x[i] + 1;                # evaluate dx/dt
-    x[i+1] = x[i] + (h*f);
+"""
+Simple Euler integration
+h=0.1 is a good default.
+"""
+function euler1(h)
+    tmax = 5;                               # max t value
+    init = 1;                               # initial condition
+    
+    t = 0:h:tmax;                           # vector of time values
+    nsteps = length(t);                     # how many steps
+    x = zeros(nsteps,1);                    # where we will store
+    # results
+    x[1] = init;
+    
+    
+    # Start the integration
+    for i=1:(nsteps-1)
+        f = t[i] - x[i] + 1;                # evaluate dx/dt
+        x[i+1] = x[i] + (h*f);
+    end
+    ## why need space?
+    hcat(t, x)
 end
 
 
-# Now plot results and compare with true solution
-
-fig, axes = subplots(1,1,figsize=(5,3))
-xtrue = @. exp(-t) + t;                    # true solution
-plot(t, xtrue, label="True")
-plot(t, x, label="Euler")
-xlabel("time (s)"); ylabel("x");
-legend(loc=2)
-
-
-
-## h comes from enclosing environement.
+function plot_euler1(;h=0.1)
+    res = euler1(h);
+    t = res[:,1]
+    x = res[:,2]
+    xtrue = @. exp(-t) + t;                    # true solution
+    
+    plot(t, hcat(x, xtrue), legend=:topleft,
+         title = "Euler integration",
+         xlabel="Time (s)", ylabel="x",
+         label=["estimate" "true"])
+end
